e1d3f641a589fbac03a1f22456dbe7d1045fe704
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-07 20:48:09 +0300

    initial source code commit

---
+++ HitCounter/HitCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRHitCount
+{
+    public class HitCounter : Hub
+    {
+        static int _hitCount;
+
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            Clients.All.onHitRecorded(_hitCount);
+        }
+
+        public override Task OnDisconnected()
+        {
+            _hitCount -= 1;
+            return Clients.All.onHitRecorded(_hitCount);
+        }
+    }
+}
---
+++ WebApiSample/WebApiSample/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiSample.Global" Language="C#" %>
